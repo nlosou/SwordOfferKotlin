9503494dc62476ff604a8de9056ac7aa1a789ae2
nlosou <2128819713@qq.com>
2025-04-05 16:02:30 +0800

    Auto commit on 2025年 04月 05日 星期六 16:02:30 CST

--- src/array/String/test.kt
+++ src/array/String/test.kt
@@ -5,4 +5,6 @@ import java.util.*
 fun main(){
     var a= "String".uppercase(Locale.getDefault())
     println(a)
+   // TODO()
+    println(83883)
 }
--- src/array/String/不含重复字符的最长子字符串.kt
+++ src/array/String/不含重复字符的最长子字符串.kt
@@ -1,2 +1,60 @@
 package array.String
 
+import kotlin.system.measureNanoTime
+import kotlin.time.measureTime
+
+
+fun sjhsjs(a:String){
+    val ans= mutableListOf<String>()
+    val array = ('a'.code..'z'.code).associateWith { 0 }.toMutableMap()
+    var i=0
+    var j=0
+    while (j<a.length){
+        val charCode = a[j].code
+        array[charCode] = array.getValue(charCode) + 1
+
+        while (array.getValue(charCode) > 1) {
+            array[a[i].code] = array.getValue(a[i].code) - 1
+            i++
+        }
+        ans.add(a.substring(i, j + 1))
+        j++
+    }
+    val res = ans.maxByOrNull { it.length } ?: ""
+    println(res)
+}
+
+fun sjhsjs2(a: String) {
+    val array = mutableMapOf<Int, Int>().withDefault { 0 }
+    val ans = mutableListOf<String>()
+    var i = 0
+    var j = 0
+
+    while (j < a.length) {
+        val charCode = a[j].code
+        array[charCode] = array.getValue(charCode) + 1
+
+        while (array.getValue(charCode) > 1) {
+            array[a[i].code] = array.getValue(a[i].code) - 1
+            i++
+        }
+
+        ans.add(a.substring(i, j + 1))
+        j++
+    }
+
+    val res = ans.maxByOrNull { it.length } ?: ""
+    println(res)
+}
+
+fun main(){
+    val a="babccabcd"
+    val text1= measureNanoTime {
+        sjhsjs(a)
+    }
+    val text2= measureNanoTime {
+        sjhsjs2(a)
+    }
+    println(text1)
+    println(text2)
+}
