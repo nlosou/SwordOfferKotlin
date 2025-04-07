a504c333a55d61263ed05139626e5536c8bdc41d
nlosou <2128819713@qq.com>
2025-04-07 20:31:18 +0800

    Auto commit on 2025年 04月 07日 星期一 20:31:18 CST

---
+++ src/array/String/有效的回文.kt
@@ -0,0 +1,28 @@
+package array.String
+
+
+
+fun sada(a:String):Boolean{
+    var i=0
+    var j=a.length-1
+    while (i<j){
+        if (!a[i].isLetter()) i++
+        if(!a[j].isLetter()) j--
+
+        if(a[i].isLetter()&&a[j].isLetter()){
+            if(a[i].uppercaseChar() !=a[j].uppercaseChar()) return false
+            else{
+                i++
+                j--
+            }
+        }
+    }
+    return true
+}
+
+
+fun main(){
+    var a="Was it a cat I saw?"
+    if(sada(a)) println("yes")
+    else println("no")
+}
